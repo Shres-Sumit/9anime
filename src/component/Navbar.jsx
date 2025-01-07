f9a3ba3d7cf69b90dedaaa46af134a5a9fce42a9
Shres-Sumit <sumitcodeshrestha123@gmail.com>
2025-01-07 17:50:41 +0545

    feat_nav : create register popup

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,27 +2,34 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ListNav from './ListNav';
 import PopupLogin from './PopupLogin';
+import PopupRegister from './PopupRegister';
 
 const Navbar = () => {
     const [showPopup, setShowPopup] = useState(false);
+    const [popupType, setPopupType] = useState('login');
 
-    const togglePopup = () => {
-        setShowPopup(!showPopup);
+    const openPopup = (type) => {
+        setPopupType(type);
+        setShowPopup(true);
     };
+
+    const closePopup = () => {
+        setShowPopup(false)
+    }
 
     return (
         <>
             <nav className="flex justify-between items-center p-5">
                 <div className="relative flex gap-7 items-center">
-                    {/* Hamburger Menu */}
+
                     <div className="relative group">
                         <Link to="#" className="text-6xl text-white hover:text-purple-900">
                             &#8801;
                         </Link>
-                        {/* Main Dropdown */}
+
                         <div className="absolute top-full left-0 hidden group-hover:flex flex-col w-32 py-2 bg-purple-900 text-white rounded-lg mt-1 shadow-lg z-10">
                             <ul className="flex flex-col gap-3 font-semibold">
-                                {/* HOME */}
+
                                 <ListNav title={'HOME'} className={''} />
                                 <ListNav title={'GENRES'} className={'group/genre'}>
                                     <p className="px-4 py-2 hover:bg-purple-950 cursor-pointer">
@@ -36,7 +43,7 @@ const Navbar = () => {
                                     </p>
                                 </ListNav>
 
-                                {/* GENRES */}
+
                                 <ListNav title={'TYPE'} className={''} />
                                 <ListNav title={'Update'} className={''} />
                             </ul>
@@ -53,16 +60,17 @@ const Navbar = () => {
                 <div>
                     <button
                         className="rounded-3xl text-white bg-purple-900 font-semibold text-lg px-3 py-2 w-28"
-                        onClick={togglePopup}
+                        onClick={() => openPopup('login')}
                     >
                         Sign in
                     </button>
                 </div>
-                <PopupLogin showPopup={showPopup} setShowPopup={setShowPopup} togglePopup={togglePopup} />
+                {popupType === 'login' ? (
+                    <PopupLogin showPopup={showPopup} openPopup={openPopup} closePopup={closePopup} />
+                ) : (
+                    <PopupRegister showPopup={showPopup} openPopup={openPopup} closePopup={closePopup} />
+                )}
             </nav>
-
-
-
         </>
     );
 };
--- src/component/PopupRegister.jsx
+++ src/component/PopupRegister.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import InputWithIcon from './singleComponent/InputWithIcon';
-import { FaLock } from 'react-icons/fa';
+import { FaUser, FaLock } from 'react-icons/fa';
 import { IoMdMail } from "react-icons/io";
 
-const PopupLogin = ({ showPopup, openPopup, closePopup }) => {
+import InputWithIcon from './singleComponent/InputWithIcon';
+
+
+const PopupRegister = ({ showPopup, openPopup, closePopup }) => {
     if (!showPopup) return null;
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
             <div className="bg-white rounded-lg p-6 w-96 shadow-lg relative">
-                <h2 className="text-xl font-bold mb-4">Sign In</h2>
+                <h2 className="text-xl font-bold mb-4">Register</h2>
                 <form>
+                    <InputWithIcon label={'Name'} type='text' placeholder='Enter your name' icon={<FaUser />} />
                     <InputWithIcon label={'Email'} type='email' placeholder='Enter your email' icon={<IoMdMail />} />
                     <InputWithIcon label={'Password'} type='password' placeholder='Enter your password' icon={<FaLock />} />
-                    <div className='mb-4'>
-                        <input type="checkbox" className='mr-4 border-none outline-none' />
-                        <span className='text-gray-700'>Remember Me</span>
-                    </div>
+                    <InputWithIcon label={' Confirm Password'} type='password' placeholder='Enter your email' icon={<FaLock />} />
+
+
+
+
+
                     <button
                         type="submit"
                         className="w-full bg-purple-900 text-white py-2 rounded-lg font-semibold hover:bg-purple-800 mb-4"
                     >
-                        Login
+                        Register
                     </button>
-
-                    <div className='flex justify-center'>
-                        <span>Don't have an account? <span className='text-purple-900 cursor-pointer hover:underline' onClick={() => openPopup('register')}> Register</span></span>
-                    </div>
                 </form>
                 <button
                     className="absolute top-2 right-2 text-gray-500 text-2xl font-semibold hover:text-gray-700"
@@ -33,9 +34,12 @@ const PopupLogin = ({ showPopup, openPopup, closePopup }) => {
                 >
                     &times;
                 </button>
+                <div className='flex justify-center cursor-pointer'>
+                    <span>Have an account? <span className='text-purple-900 hover:underline '>Sign-in</span></span>
+                </div>
             </div>
         </div>
     );
 };
 
-export default PopupLogin;
+export default PopupRegister;
