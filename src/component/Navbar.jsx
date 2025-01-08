5def25cba211887494d839a524054e691447e520
Shres-Sumit <sumitcodeshrestha123@gmail.com>
2025-01-08 06:37:19 +0545

    branches deleted

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -100,9 +100,6 @@ const Navbar = () => {
                     }
                 </div>
 
-
-
-
                 <div>
                     <button
                         className="rounded-3xl text-white bg-purple-900 font-semibold text-lg px-3 py-2 w-28"
